9a5646d2a4d8031bb3c0a0377e4cdfd3702b653d
mugiwara404 <36692871+mugiwara404@users.noreply.github.com>
2020-01-25 23:26:31 +0700

    Add files via upload

---
+++ SoalBootcampProjek/book.sql
@@ -0,0 +1,140 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 25, 2020 at 11:57 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `book`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book_tb`
+--
+
+CREATE TABLE `book_tb` (
+  `id_book` int(11) NOT NULL,
+  `name_book` varchar(255) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `writer_id` int(11) NOT NULL,
+  `publiction_year` int(11) NOT NULL,
+  `img` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `book_tb`
+--
+
+INSERT INTO `book_tb` (`id_book`, `name_book`, `category_id`, `writer_id`, `publiction_year`, `img`) VALUES
+(1, 'The Pragmatic Programmer', 1, 1, 1999, 'book1.png'),
+(2, 'Structure and Interpretation of Computer Programs', 1, 2, 1979, 'book2.png'),
+(3, 'A Book of Myths', 3, 3, 1914, 'book3.png'),
+(4, 'Norse Gods and Giants', 3, 4, 1967, 'book4.png'),
+(5, 'The Illustrated Book of Myths: Tales & Legends of the World', 3, 5, 1995, 'book5.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category_tb`
+--
+
+CREATE TABLE `category_tb` (
+  `id_cat` int(11) NOT NULL,
+  `name_cat` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `category_tb`
+--
+
+INSERT INTO `category_tb` (`id_cat`, `name_cat`) VALUES
+(1, 'Programming Book'),
+(2, 'Fiksi'),
+(3, 'Myth');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `writer_tb`
+--
+
+CREATE TABLE `writer_tb` (
+  `id_writ` int(11) NOT NULL,
+  `name_writ` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `writer_tb`
+--
+
+INSERT INTO `writer_tb` (`id_writ`, `name_writ`) VALUES
+(1, 'Andy Hunt'),
+(2, 'Hal Abelson'),
+(3, 'Jean Lang'),
+(4, 'Ingri and Edgar Parin d\'Aulaire'),
+(5, 'Neil Philip');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `book_tb`
+--
+ALTER TABLE `book_tb`
+  ADD PRIMARY KEY (`id_book`);
+
+--
+-- Indexes for table `category_tb`
+--
+ALTER TABLE `category_tb`
+  ADD PRIMARY KEY (`id_cat`);
+
+--
+-- Indexes for table `writer_tb`
+--
+ALTER TABLE `writer_tb`
+  ADD PRIMARY KEY (`id_writ`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `book_tb`
+--
+ALTER TABLE `book_tb`
+  MODIFY `id_book` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `category_tb`
+--
+ALTER TABLE `category_tb`
+  MODIFY `id_cat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `writer_tb`
+--
+ALTER TABLE `writer_tb`
+  MODIFY `id_writ` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
